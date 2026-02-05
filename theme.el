bec125ff3635ce0e4f4077d3c002ac4f40186ca8
Evan Delepine-Gengembre <evan.delepine-gengembre.etu@univ-lille.fr>
2026-02-05 22:09:35 +0100

    Theme modus custom avec le violet en couleur d'accent

--- theme.el
+++ theme.el
@@ -2,5 +2,25 @@
 ;; (load-theme 'modus-vivendi)
 
 
-(load-theme 'gruvbox-dark-medium)
+;; (load-theme 'gruvbox-dark-medium)
 
+(setq modus-themes-italic-constructs t
+      modus-themes-bold-constructs t
+      modus-themes-mixed-fonts t ; Utilise la variable-pitch pour Org-mode
+      modus-themes-variable-pitch-ui t) ; Interface plus douce
+
+(setq modus-themes-common-palette-overrides
+      '((cursor "#bd93f9")
+        (fg-line-number-active "#bd93f9")
+        (border-focused "#bd93f9")
+        (heading-1 "#bd93f9")
+        (bg-region "#44475a") 
+        (accent-0 "#bd93f9")
+        (accent-1 "#bd93f9")))
+
+(load-theme 'modus-vivendi t)
+
+(custom-set-faces
+ '(org-level-1 ((t (:inherit outline-1 :height 1.2 :foreground "#bd93f9" :weight bold))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.1 :foreground "#bd93f9"))))
+ '(org-document-title ((t (:height 1.5 :foreground "#bd93f9" :weight bold))))) 
